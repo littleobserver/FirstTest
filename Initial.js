3308657b28bbb93e970915cd7d8d10d124ada74b
littleobserver <aleksandra.misiewicz@gmail.com>
2016-12-22 20:23:26 +0100

    ccc

--- Initial.js
+++ Initial.js
@@ -1,4 +1,34 @@
 function showText ()
 {
-	document.write('Little Observer starts to code');
+	document.write(myAdd());
+}
+
+function myAdd()
+{
+	var a = "Ala ma kota";
+	var y = a.charAt(7);
+ 
+	document.write(y);
+	
+}
+
+function Swap (txt)
+{
+	for (i=0; i <= txt.length; i++)
+	{
+		if (txt[i] = "a")
+		{
+			var res = txt.replace("a","b");
+			txt = res;
+
+		}
+		else
+		{
+		window.alert("you moron");
+		}
+		
+	}
+		//return(txt);
+		document.write(res);
+	
 }
